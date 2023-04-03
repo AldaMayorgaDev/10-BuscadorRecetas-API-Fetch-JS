806c86a90b874aba992c8d5f8e3ae173b61d9091
Aldahir Mayorga <samg.25sep@gmail.com>
2023-04-03 15:03:16 -0600

    Se itera la respuesta del endpoint y se crea una card por cada receta, se añade una funcion para limpiar el html previo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 function inicarApp(){
     const selectCategorias = document.querySelector('#categorias');
+    selectCategorias.addEventListener('change', selectCategoria);
+
+    const divResultado = document.querySelector('#resultado');
 
     //Obtener todas las categorias de la API, usando el siguiente endpoint www.themealdb.com/api/json/v1/1/categories.php
     
@@ -43,6 +46,87 @@ function inicarApp(){
                 selectCategorias.appendChild(option);
             })
 
+    }
+
+    function selectCategoria(e) {
+        console.log('e.target.value :>> ', e.target.value);
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+        console.log('url :>> ', url);
+
+        /* Consumiendo API que filtra los resultados por categoria  a traves del endpoint https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood*/
+
+        fetch(url)
+            .then(respuesta =>{
+                //console.log('respuesta :>> ', respuesta);
+                return respuesta.json();
+            })
+            .then(resultado=>{
+                console.log('resultado :>> ', resultado);
+                mostrarRecetas(resultado.meals);
+            })
+    }
+
+    function mostrarRecetas(recetas =[]) {
+
+        //impiar html previo
+        limpiarHTML(divResultado);
+
+        const heading = document.createElement('H2');
+        heading.classList.add('text-center', 'text-black', 'my-5');
+        heading.textContent = recetas.length ? 'Resultados' : 'No Hay Resultados';
+
+        divResultado.appendChild(heading);
+        //Iterar los resultados
+
+        recetas.forEach( receta =>{
+
+            const {idMeal, strMeal, strMealThumb}= receta;
+            const recetaContenedor = document.createElement('DIV');
+            recetaContenedor.classList.add('col-md-4');
+            //console.log('recetaContenedor :>> ', recetaContenedor);
+
+            const recetaCard = document.createElement('DIV');
+            recetaCard.classList.add('card', 'mb-4')
+            //console.log('recetaCard :>> ', recetaCard);
+
+            const recetaImagen = document.createElement('IMG');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt = `Imagen de la receta ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+
+            const recetaCardBody = document.createElement('DIV');
+            recetaCardBody.classList.add('card-body');
+
+            const recetaHeading = document.createElement('H3');
+            recetaHeading.classList.add('card-title', 'mb-3');
+            recetaHeading.textContent = strMeal;
+
+            const recetaButton = document.createElement('BUTTON');
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = 'Ver Receta';
+
+
+            //Insertar receta en el HTML
+            recetaCardBody.appendChild(recetaHeading);
+            recetaCardBody.appendChild(recetaButton);
+
+            recetaCard.appendChild(recetaImagen);
+            recetaCard.appendChild(recetaCardBody);
+
+            recetaContenedor.appendChild(recetaCard);
+
+
+            divResultado.appendChild(recetaContenedor);
+        })
+        
+    }
+
+    function limpiarHTML (selector){
+        while(selector.firstChild){
+            selector.removeChild(selector.firstChild);
+        }
     }
 }
 
